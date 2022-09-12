17cdac632d9a17b8e037981a8f0183188c84752f
Frederick Thiemer <fthiemer@techfak.uni-bielefeld.de>
2022-09-13 01:59:14 +0200

    Close #5, close #6: Add buy functionality

--- PsyCurioShopScene/Assets/Scripts/BuyableObject.cs
+++ PsyCurioShopScene/Assets/Scripts/BuyableObject.cs
@@ -1,8 +1,31 @@
+using System;
 using UnityEngine;
 
-public class BuyableObject : MonoBehaviour
-{
+[RequireComponent(typeof(MeshRenderer))]
+public class BuyableObject : MonoBehaviour {
+
+
+    /// <summary>
+    /// The distance between center and lower edge. Used for correct placement on the counter.
+    /// </summary>
+    public float YOffset { get; private set; }
+
+    [SerializeField] private string name;
+    public string Name { get { return name; } }
+    
+    [SerializeField] private float price;
+    public float Price { get { return price; } }
+    
+    [SerializeField] private Counter counter;
+
+
+    private void Awake() {
+        var tmpMeshFilter = gameObject.GetComponent<MeshRenderer>();
+        YOffset = tmpMeshFilter.bounds.extents.y;
+    }
+
     private void OnMouseDown() {
-        Debug.Log("BuyableObject");
+        counter.PlaceOnCounter(gameObject);
+        Debug.Log($"Bought {Name} for {Price}");
     }
 }
